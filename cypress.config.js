ab5c765d526b2cbfa823c5d2fe48b58964acac72
akan-baba <akan_antia@live.co.uk>
2024-11-19 08:56:16 +0000

    Allure Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
     },
+    env: {
+      allure: true,
+      allureReuseAfterSpec: true,
+      allureResultsPath: './allure-results', // Optional: Change the output directory
+    },
+    baseUrl: 'http://192.168.1.201:8085/',
+    retries: {
+      runMode: 2, // Retry failing tests twice in headless mode
+      openMode: 0, // No retries in interactive mode
+    }
+
+    
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,9 +2,13 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@shelex/cypress-allure-plugin';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+require('@shelex/cypress-allure-plugin');
+
+
 const app = window.top;
 if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
 const style = app.document.createElement('style');
@@ -13,3 +17,20 @@ style.innerHTML =
 style.setAttribute('data-hide-command-log-request', '');
 app.document.head.appendChild(style);
 }
+
+
+Cypress.on('fail', (error, runnable) => {
+    cy.allure().testBody(error.stack); // Attach error stack to the Allure report
+    throw error; // Still fail the test
+  });
+
+
+const skipFailures = (Cypress.env('skipFailures') === 'true');
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Prevent failing the test on uncaught exceptions
+  if (skipFailures) {
+    cy.allure().testBody(err.stack); // Attach the exception to the report
+    return false;
+  }
+});
